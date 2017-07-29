19a1a5be98d28874f2d08dd1e76c690e52bb86fa
kjhsto <kudutetu@yahoo.co.jp>
2017-07-29 11:35:20 +0900

    click

--- js/clicker.js
+++ js/clicker.js
@@ -1,7 +1,27 @@
-var i=0;
+/* 敵へダメージの合計 */
+var damage=0;
+/* 敵へダメージ */
 var num=0;
+
+/* クリックしたときのカウント加速 */
 function count(num) {
-  i = i + num;
+  damage = damage + num;
   target = document.getElementById("canvas_up");
-  target.innerHTML = i;
+  target.innerHTML = damage;
 }
+
+if (navigator.cookieEnabled)  // cookieが使えるか確認
+{
+  document.getElementById("canvas_down").innerHTML = "▽クッキーが有効です！データが保存できるよ！";
+}
+else
+{
+  document.getElementById("canvas_down").innerHTML = "▽クッキー無効！データが消えちゃうよ(´・ω・`)";
+}
+
+//function cookie_upload(){
+//  document.cookie = 'data=' + encodeURIComponent( damage );
+//}
+
+/* 一分ごとにクッキー保存 */
+setInterval("count(1)",60000);
